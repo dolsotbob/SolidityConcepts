115ee7cba31a1c5d94e46a868b3f8d87b8251d8f
dolsotbob <1hyoon3@naver.com>
2025-04-02 11:26:22 +0900

    updated

--- contracts/SolidityConcepts.sol
+++ contracts/SolidityConcepts.sol
@@ -1,1 +1,50 @@
-contract SolidityConcepts {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SolidityConcepts {
+    uint256 public constant FIXED_VALUE = 100;
+    uint256 public value = 50;
+    address public immutable owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function checkValue(uint256 _value) public pure returns (string memory) {
+        if (_value > 100) {
+            return "Value is greater than 100";
+        } else if (_value == 100) {
+            return "Value is exactly 100";
+        } else {
+            return "Value is less than 100";
+        }
+    }
+
+    function sumUpTo(uint256 _number) public pure returns (uint256) {
+        uint256 sum = 0;
+
+        for (uint256 i = 1; i <= _number; i++) {
+            unchecked {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    event ValueChanged(uint256 oldValue, uint256 newValue);
+
+    function updateValue(uint256 newValue) public onlyOwner {
+        uint256 oldValue = value;
+        value = newValue;
+        emit ValueChanged(oldValue, newValue);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
+    }
+
+    function ownerFunction() public view onlyOwner returns (string memory) {
+        return "Hello, Owner!";
+    }
+}
