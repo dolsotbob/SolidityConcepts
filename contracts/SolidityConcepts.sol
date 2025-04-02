a2a17d4b17497a0d4b16df394a222db87e5ecc6d
dolsotbob <1hyoon3@naver.com>
2025-04-02 16:35:12 +0900

    modified

--- contracts/SolidityConcepts.sol
+++ contracts/SolidityConcepts.sol
@@ -34,14 +34,14 @@ contract SolidityConcepts {
     event ValueChanged(uint256 oldValue, uint256 newValue);
 
     function updateValue(uint256 newValue) public onlyOwner {
-        uint256 oldValue = value;
+        uint256 oldValue = value; //oldValue는 function 안에 있는 로컬 변수이고 로컬에 저장됨
         value = newValue;
         emit ValueChanged(oldValue, newValue);
     }
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Not the contract owner");
-        _;
+        _; // onlyOwner가 들어간 함수의 실제 코드 실행(여기선 아래 함수)
     }
 
     function ownerFunction() public view onlyOwner returns (string memory) {
@@ -50,17 +50,22 @@ contract SolidityConcepts {
 
     receive() external payable {}
 
-    function sendEther(address payable recipient) public payable {
+    function sendEther(address payable _to) public payable {
         require(msg.value > 0, "Must send ether");
-        recipient.transfer(msg.value);
+        (bool success, ) = _to.call{value: msg.value}("");
+        require(success, "Failed to send ether");
     }
 
     function getContractBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
+    // 실무에서는 정말로 받아지는지 반드시 확인 하고 넘어갸야 함(테스크 많이 해볼수록 좋음)
+    //call 쓰면 require 쓴다
     function withDraw() public onlyOwner {
-        require(address(this).balance > 0, "No funds to withdraw");
-        payable(owner).transfer(address(this).balance);
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success, "Failed to withdraw");
+        // require(address(this).balance > 0, "No funds to withdraw");
+        // payable(owner).transfer(address(this).balance);
     }
 }
